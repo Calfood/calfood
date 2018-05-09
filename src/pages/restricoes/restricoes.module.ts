765bcac97fcd03f60150734646e72744c9ab19bc
Tati <tatianecoutinho2805@gmail.com>
2018-05-08 21:58:08 -0300

    first commit

---
+++ src/pages/restricoes/restricoes.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { RestricoesPage } from './restricoes';
+
+@NgModule({
+  declarations: [
+    RestricoesPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(RestricoesPage),
+  ],
+})
+export class RestricoesPageModule {}
